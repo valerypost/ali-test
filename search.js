a23fabfa916004c3e24fd808827dad4c55ec5d99
ValeriyBalovnev <Valeriy.Balonev@blastradius.com>
2017-03-25 14:32:41 -0400

    t2

--- search.js
+++ search.js
@@ -2,31 +2,35 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs')
 var q = require('q');
-var searchTerm = 'smd';
-var url = 'https://www.aliexpress.com/wholesale?SearchText=' + searchTerm;
-var pagination='www.aliexpress.com/wholesale?initiative';
+var searchTerm = '';
+var url = 'https://www.aliexpress.com/wholesale?SortType=total_tranpro_desc&SearchText=' + searchTerm;
+var pagination='page=';
 var shiping = 'https://freight.aliexpress.com/ajaxFreightCalculateService.htm?callback=jQuery18303278056892461083_1490202654424&f=d&count=1&currencyCode=USD&sendGoodsCountry=&country=CA&province=&city=&abVersion=1&_=1490202775139&productid=';
-var pages=[];
-// pages.push(url);
+
  var itemCounter=0;
 var re = /([\w\d_-]*)\.?[^\\\/]*$/i;
-var requests=[];
-var pages=[];
+
+
 console.log("start");
 
 exports.generateSearchResult= function (searchPhase,res) {
+    var pages=[];
+    var requests=[];
+    pages.push(url+searchPhase);
     res.write("!!!!!!!!!!!!!"+searchPhase);
     var htmlOut="start !!!!!!!!!!!!!";
     request(url+searchPhase, function(err, resp, body){
         $ = cheerio.load(body);
         var links = $('a'); //jquery get all hyperlinks
-        console.log(links.length);
-        // console.log(":::"+JSON.stringify($(links)));
+        console.log("page links "+links.length);
+        //console.log(":::"+JSON.stringify($(links)));
         $(links).each(function(i, link){
+
             var linkstxt=$(link).text();
             var href=$(link).attr('href');
+           // console.log("link" + ':  ' + href);
 
-            if(href.includes(pagination)) {
+            if(href.indexOf(pagination)>0) {
 
                 href=href.replace("//","https://");
                 console.log("linkstxt" + ':\n  ' + href);
@@ -39,6 +43,7 @@ exports.generateSearchResult= function (searchPhase,res) {
 
 
         pages.forEach(function (pageUrl) {
+            console.log("search:"+pageUrl)
             requests.push(processDetail(pageUrl));
         });
         // console.log("counter:" + itemCounter);
@@ -99,7 +104,7 @@ exports.generateSearchResult= function (searchPhase,res) {
                         //console.log("+");
                         var productId=href.match(re)[1];
                         //console.log("productId:" +productId);
-                        pages.push(checkProduct(href,linkstxt, productId));
+                        pages.push(checkProduct(href,linkstxt, productId,pageUrl));
                     }
                 });
                 d.resolve("ok");
@@ -111,7 +116,7 @@ exports.generateSearchResult= function (searchPhase,res) {
         return d.promise;
         // console.log("linkstxt" + pageUrl);
     }
-    function checkProduct(urlPage,name, productId) {
+    function checkProduct(urlPage,name, productId,pageUrl) {
         var p = q.defer();
         if(name) {
             request.get(shiping + productId)
@@ -121,6 +126,7 @@ exports.generateSearchResult= function (searchPhase,res) {
                         var startParam=urlPage.indexOf('?')
                         urlPage=urlPage.substring(0,startParam);
                         var htmlA = '<a  href="' + urlPage + '">' + name + '</a>';
+                        console.log(pageUrl);
                         console.log(htmlA);
                         res.write(htmlA+'</br>');
                         p.resolve("epacket");
